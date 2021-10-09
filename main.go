9bcd1b199135f2a8c468babdc60f49fc735d0393
kobulwidodo <fadhilhan01@gmail.com>
2021-10-09 15:08:15 +0700

    Setup Database

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"bwastartup/config"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("halo")
+	var APP_ENV = os.Getenv("APP_ENV")
+	if APP_ENV != "PRODUCTION" {
+		if err := godotenv.Load(); err != nil {
+			log.Fatal("Error loading .env file")
+		}
+	}
+
+	// Setup Database
+	_, err := config.InitDB()
+	if err != nil {
+		fmt.Println(err)
+		panic("Something wrong with database")
+	}
+	fmt.Println("Sukses connect ke database!")
 }
